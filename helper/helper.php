b8014da2eefcaad778015a18603aa12c73ccd3b2
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2026-02-23 16:41:58 +0530

    Render bulk translate button only post & page.
    Render bulk translate button only post & page.

--- helper/helper.php
+++ helper/helper.php
@@ -23,15 +23,19 @@ class Helper {
         if((isset($current_screen->action) && $current_screen->action === 'add') || (isset($_GET['post']) && isset($_GET['action']) && sanitize_text_field(wp_unslash($_GET['action'])) === 'edit')){
             return false;
         }
-
+        
         if(!isset($current_screen->post_type) || empty($current_screen->post_type)){
             return false;
         }
-
+        
         if(isset($current_screen->post_type) && $current_screen->post_type === 'attachment' ){
             return false;
         }
 
+        if(!in_array($current_screen->post_type, array("page", "post"))){
+            return false;
+        }
+        
         return true;
     }
 
