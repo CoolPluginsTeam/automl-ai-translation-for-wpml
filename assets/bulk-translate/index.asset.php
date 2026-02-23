4082ad05351cf1f3a850cf861162ca1746370bd3
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2026-02-23 13:06:09 +0530

    Skip script & style tag content translation.
    Skip script & style tag content translation.

--- assets/bulk-translate/index.asset.php
+++ assets/bulk-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7b3ef3683d678092354d');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd8948118d90ad40ca5ad');
