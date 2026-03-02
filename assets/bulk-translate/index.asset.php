856f35dd17ca9e5a5ec8101ea2a182bbad5bf4bd
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2026-03-02 17:25:41 +0530

    Improved page translation post not found error message.
    Improved page translation post not found error message.

--- assets/bulk-translate/index.asset.php
+++ assets/bulk-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '27612232f86ceff828c3');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'fcdf09352d819098fba8');
