0304d3ec5982e7839168c23c041a920b60d51801
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2026-03-02 14:57:57 +0530

    Fixed elementor page content update issue.
    Fixed elementor page content update issue.

--- includes/wpml/builder/elementor/elementor-update.php
+++ includes/wpml/builder/elementor/elementor-update.php
@@ -69,9 +69,10 @@ class Elementor_Update extends Content_Update_Base {
 
 		$automl_wpml_elementor_data = get_post_meta( $this->post_id, '_elementor_data', true );
 
-		if ( ! is_array( $automl_wpml_elementor_data ) || empty( $automl_wpml_elementor_data ) ) {
+		if ( empty( $automl_wpml_elementor_data ) ) {
 			return;
 		}
+
 
 		$this->elementor_builder_factory->update_translated_post( $this->editor_type, $this->translated_post_id, $source_post, $this->translate_strings, $this->target_language );
 
