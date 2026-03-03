ea786c53bffb78bd77177485fc389d57fedd45e6
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2026-03-03 12:23:23 +0530

    Improved error message styling.
    1. Improved error message styling in page translation modal box.

--- assets/bulk-translate/index.asset.php
+++ assets/bulk-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b80b2dd2538bfc897385');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '3c95a6cf517f7b166f8d');
