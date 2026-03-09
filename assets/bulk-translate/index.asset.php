bb122cee5b18cc9c373bbd50f6be4de5fe757c97
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2026-03-09 16:35:32 +0530

    update build files.
    update build files.

--- assets/bulk-translate/index.asset.php
+++ assets/bulk-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '09980e123bcffb2065ee');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '2040b5cb33169d6d326b');
