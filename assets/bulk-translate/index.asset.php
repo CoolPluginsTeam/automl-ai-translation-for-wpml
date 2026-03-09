660d2515bb86505d84e33946e5577558bb895486
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2026-03-09 16:21:16 +0530

    Improved error modal box styling.
    Improved error modal box styling.

--- assets/bulk-translate/index.asset.php
+++ assets/bulk-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7b96dcad24d29e3cb5d3');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '09980e123bcffb2065ee');
